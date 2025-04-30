92a71eb3eb978f0b6cdebd2c2585e07462f0c8e5
VaibhavKrishna0607 <vaibhavkrishna0607@gmail.com>
2025-04-30 11:14:29 +0530

    donee

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import books from "./booksData"; // Import book data
 
 const Home = () => {
   return (
@@ -7,7 +8,18 @@ const Home = () => {
       <Link to="/add-book">
         <button>Add Book</button>
       </Link>
-      {/* List of BookCard components */}
+
+      {/* Display list of books */}
+      <div>
+        {books.map((book) => (
+          <div key={book.id} style={{ border: "1px solid #ccc", margin: "10px", padding: "10px" }}>
+            <h2>{book.title}</h2>
+            <p><strong>Author:</strong> {book.author}</p>
+            <p>{book.description}</p>
+            <img src={book.coverImage} alt={book.title} width="100" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
